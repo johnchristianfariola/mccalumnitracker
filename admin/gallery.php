863e5cf5092b9508ddb55c64e4a25b1256738e39
Histsugaya2003 <johnchristianfariola@gmail.com>
2024-06-21 21:12:05 +0800

    commit

--- admin/gallery.php
+++ admin/gallery.php
@@ -36,7 +36,7 @@ $galleries = json_decode($galleryData, true) ?: [];
 
                     <div class="search-container">
                         <input type="text" class="search-input" id="search-input" placeholder="Search...">
-                        <button class="search-button" onclick="filterTable()">
+                        <button class="search-button" onclick="filterDivs()">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                 stroke-linejoin="round" class="feather feather-search">
@@ -98,7 +98,7 @@ $galleries = json_decode($galleryData, true) ?: [];
                                                         </svg>
                                                     </div>
                                                     <div class="dropdown-menu" id="album<?php echo $id; ?>">
-                                
+
                                                         <div class="dropdown-item open-modal"
                                                             data-id="<?php echo htmlspecialchars($id); ?>"
                                                             onclick="event.stopPropagation(); openEditModal('<?php echo $id; ?>')">
