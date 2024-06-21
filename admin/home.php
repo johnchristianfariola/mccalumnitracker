f6260726d3d337c92d2b8ed44823e3f74f2d9fdb
Histsugaya2003 <johnchristianfariola@gmail.com>
2024-06-22 07:52:26 +0800

    commit time 7:52

--- admin/home.php
+++ admin/home.php
@@ -1,191 +1,218 @@
-<?php include 'includes/session.php'; ?>
 <?php 
-  include 'includes/timezone.php'; 
-  $today = date('Y-m-d');
-  $year = date('Y');
-  if(isset($_GET['year'])){
-    $year = $_GET['year'];
+include 'includes/session.php'; 
+?>
+<?php
+include 'includes/timezone.php';
+$today = date('Y-m-d');
+$year = date('Y');
+if (isset($_GET['year'])) {
+  $year = $_GET['year'];
+}
+
+require_once 'includes/firebaseRDB.php';
+
+// Your Firebase Realtime Database URL
+$databaseURL = "https://mccnians-bc4f4-default-rtdb.firebaseio.com/";
+
+// Create an instance of the firebaseRDB class
+$firebase = new firebaseRDB($databaseURL);
+
+// Fetch courses, departments, batch years, and alumni data from Firebase
+$alumniData = $firebase->retrieve("alumni");
+$forumData = $firebase->retrieve("forum");
+$jobData = $firebase->retrieve("job");
+$eventData = $firebase->retrieve("event");
+
+// Decode JSON data into associative arrays
+$alumni = json_decode($alumniData, true) ?: [];
+$forumData = json_decode($forumData, true) ?: [];
+$jobData = json_decode($jobData, true) ?: [];
+$eventData = json_decode($eventData, true) ?: [];
+
+
+$alumniCount = 0;
+if (is_array($alumni)) {
+  $alumniCount = count($alumni);
+}
+
+$jobCount = 0;
+if (is_array($jobData)) {
+  foreach ($jobData as $job) {
+    if (isset($job['status']) && $job['status'] === 'Active') {
+      $jobCount++;
+    }
   }
+}
+
+$eventCount = 0;
+if (is_array($alumni)) {
+  $eventCount = count($eventData);
+}
+
+
 ?>
 <?php include 'includes/header.php'; ?>
+
 <body class="hold-transition skin-blue sidebar-mini">
-<div class="wrapper">
- 
-  <?php include 'includes/navbar.php'; ?>
-  <?php include 'includes/menubar.php'; ?>
+  <div class="wrapper">
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header box-header-background box-padding">
-      <h1>
-        Dashboard
-      </h1>
+    <?php include 'includes/navbar.php'; ?>
+    <?php include 'includes/menubar.php'; ?>
 
-      
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Dashboard</li>
-      </ol>
-    </section>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <section class="content-header box-header-background box-padding">
+        <h1>
+          Dashboard
+        </h1>
 
-    <!-- Main content -->
-    <section class="content">
-      <?php
-        if(isset($_SESSION['error'])){
+        <ol class="breadcrumb">
+          <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+          <li class="active">Dashboard</li>
+        </ol>
+      </section>
+
+      <!-- Main content -->
+      <section class="content">
+        <?php
+        if (isset($_SESSION['error'])) {
           echo "
             <div class='alert alert-danger alert-dismissible'>
               <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
               <h4><i class='icon fa fa-warning'></i> Error!</h4>
-              ".$_SESSION['error']."
+              " . $_SESSION['error'] . "
             </div>
           ";
           unset($_SESSION['error']);
         }
-        if(isset($_SESSION['success'])){
+        if (isset($_SESSION['success'])) {
           echo "
             <div class='alert alert-success alert-dismissible'>
               <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
               <h4><i class='icon fa fa-check'></i> Success!</h4>
-              ".$_SESSION['success']."
+              " . $_SESSION['success'] . "
             </div>
           ";
           unset($_SESSION['success']);
         }
-      ?>
-      <!-- Small boxes (Stat box) -->
-      <div class="row" >
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box" style="background: linear-gradient(to right, #ffbf96, #fe7096) !important;">
+        ?>
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+          <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box" style="background: linear-gradient(to right, #ffbf96, #fe7096) !important;">
             <div class="inner">
-              <?php
-                $sql = "SELECT * FROM books";
-                $query = $conn->query($sql);
-
-                echo "<h3>".$query->num_rows."</h3>";
-              ?>
-
-              <p>Total Of Alumni</p>
+                <h3><?php echo $alumniCount; ?></h3>
+                <p>Total Alumni</p>
+              </div>
+              <div class="icon">
+                <i class="fa fa-graduation-cap"></i>
+              </div>
+              <a href="book.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
             </div>
-            <div class="icon">
-              <i class="fa fa-graduation-cap"></i>
-            </div>
-            <a href="book.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box" style="background: linear-gradient(to right, #90caf9, #047edf 99%) !important;">
-            <div class="inner">
-              <?php
+          <!-- ./col -->
+          <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box" style="background: linear-gradient(to right, #90caf9, #047edf 99%) !important;">
+              <div class="inner">
+                <?php
                 $sql = "SELECT * FROM students";
                 $query = $conn->query($sql);
 
-                echo "<h3>".$query->num_rows."</h3>";
-              ?>
-          
-              <p>Forum Topic</p>
+                echo "<h3>" . $query->num_rows . "</h3>";
+                ?>
+
+                <p>Forum Topic</p>
+              </div>
+              <div class="icon">
+                <i class="fa fa-weixin"></i>
+              </div>
+              <a href="student.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
             </div>
-            <div class="icon">
-              <i class="fa fa-weixin"></i>
-            </div>
-            <a href="student.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box" style="background: linear-gradient(to right, #84d9d2, #07cdae) !important">
+          <!-- ./col -->
+          <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box" style="background: linear-gradient(to right, #84d9d2, #07cdae) !important">
             <div class="inner">
-              <?php
-                $sql = "SELECT * FROM returns WHERE date_return = '$today'";
-                $query = $conn->query($sql);
-
-                echo "<h3>".$query->num_rows."</h3>";
-              ?>
-             
-              <p>Active Job</p>
+                <h3><?php echo $jobCount; ?></h3>
+                <p>Active Job</p>
+              </div>
+              <div class="icon">
+                <i class="fa fa-tasks"></i>
+              </div>
+              <a href="return.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
             </div>
-            <div class="icon">
-              <i class="fa fa-tasks"></i>
-            </div>
-            <a href="return.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box" style="background: linear-gradient(to right, #f6e384, #ffd500) !important;">
+          <!-- ./col -->
+          <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box" style="background: linear-gradient(to right, #f6e384, #ffd500) !important;">
             <div class="inner">
-              <?php
-                $sql = "SELECT * FROM borrow WHERE date_borrow = '$today'";
-                $query = $conn->query($sql);
-
-                echo "<h3>".$query->num_rows."</h3>";
-              ?>
-
-              <p>Event</p>
+                <h3><?php echo $eventCount; ?></h3>
+                <p>Event</p>
+              </div>
+     
+              <div class="icon">
+                <i class="fa fa-clock-o"></i>
+              </div>
+              <a href="borrow.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
             </div>
-            <div class="icon">
-              <i class="fa fa-clock-o"></i>
-            </div>
-            <a href="borrow.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
+          <!-- ./col -->
         </div>
-        <!-- ./col -->
-      </div>
-      <!-- /.row -->
-      <div class="row">
-        <div class="col-xs-12">
-          <div class="box">
-            <div class="box-header with-border">
-              <h3 class="box-title" style="color:white;">Alumni Report </h3>
-              <div class="box-tools pull-right">
-                <form class="form-inline">
-                  <div class="form-group">
-                    <label style="color:white;">Select Year: </label>
-                    <select class="form-control input-sm" style="height:25px; font-size:10px" id="select_year">
-                      <?php
-                        for($i=2015; $i<=2065; $i++){
-                          $selected = ($i==$year)?'selected':'';
+        <!-- /.row -->
+        <div class="row">
+          <div class="col-xs-12">
+            <div class="box">
+              <div class="box-header with-border">
+                <h3 class="box-title" style="color:white;">Alumni Report </h3>
+                <div class="box-tools pull-right">
+                  <form class="form-inline">
+                    <div class="form-group">
+                      <label style="color:white;">Select Year: </label>
+                      <select class="form-control input-sm" style="height:25px; font-size:10px" id="select_year">
+                        <?php
+                        for ($i = 2015; $i <= 2065; $i++) {
+                          $selected = ($i == $year) ? 'selected' : '';
                           echo "
-                            <option value='".$i."' ".$selected.">".$i."</option>
+                            <option value='" . $i . "' " . $selected . ">" . $i . "</option>
                           ";
                         }
-                      ?>
-                    </select>
-                  </div>
-                </form>
+                        ?>
+                      </select>
+                    </div>
+                  </form>
+                </div>
               </div>
-            </div>
-            <div class="box-body">
-              <div class="chart">
-                <br>
-                <div id="legend" class="text-center"></div>
-                <canvas id="barChart" style="height:350px"></canvas>
+              <div class="box-body">
+                <div class="chart">
+                  <br>
+                  <div id="legend" class="text-center"></div>
+                  <canvas id="barChart" style="height:350px"></canvas>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
 
       </section>
       <!-- right col -->
     </div>
-  	<?php include 'includes/footer.php'; ?>
+    <?php include 'includes/footer.php'; ?>
 
-</div>
-<!-- ./wrapper -->
+  </div>
+  <!-- ./wrapper -->
 
-<!-- Chart Data -->
-<?php
-  $and = 'AND YEAR(date) = '.$year;
+  <!-- Chart Data -->
+  <?php
+  $and = 'AND YEAR(date) = ' . $year;
   $months = array();
   $return = array();
   $borrow = array();
-  for( $m = 1; $m <= 12; $m++ ) {
+  for ($m = 1; $m <= 12; $m++) {
     $sql = "SELECT * FROM returns WHERE MONTH(date_return) = '$m' AND YEAR(date_return) = '$year'";
     $rquery = $conn->query($sql);
     array_push($return, $rquery->num_rows);
@@ -194,8 +221,8 @@
     $bquery = $conn->query($sql);
     array_push($borrow, $bquery->num_rows);
 
-    $num = str_pad( $m, 2, 0, STR_PAD_LEFT );
-    $month =  date('M', mktime(0, 0, 0, $m, 1));
+    $num = str_pad($m, 2, 0, STR_PAD_LEFT);
+    $month = date('M', mktime(0, 0, 0, $m, 1));
     array_push($months, $month);
   }
 
@@ -203,80 +230,81 @@
   $return = json_encode($return);
   $borrow = json_encode($borrow);
 
-?>
-<!-- End Chart Data -->
-<?php include 'includes/scripts.php'; ?>
-<script>
-$(function(){
-  var barChartCanvas = $('#barChart').get(0).getContext('2d')
-  var barChart = new Chart(barChartCanvas)
-  var barChartData = {
-    labels  : <?php echo $months; ?>,
-    datasets: [
-      {
-        label               : 'Borrow',
-        fillColor           : 'rgba(210, 214, 222, 1)',
-        strokeColor         : 'rgba(210, 214, 222, 1)',
-        pointColor          : 'rgba(210, 214, 222, 1)',
-        pointStrokeColor    : '#c1c7d1',
-        pointHighlightFill  : '#fff',
-        pointHighlightStroke: 'rgba(220,220,220,1)',
-        data                : <?php echo $borrow; ?>
-      },
-      {
-        label               : 'Return',
-        fillColor           : 'rgba(60,141,188,0.9)',
-        strokeColor         : 'rgba(60,141,188,0.8)',
-        pointColor          : '#3b8bba',
-        pointStrokeColor    : 'rgba(60,141,188,1)',
-        pointHighlightFill  : '#fff',
-        pointHighlightStroke: 'rgba(60,141,188,1)',
-        data                : <?php echo $return; ?>
+  ?>
+  <!-- End Chart Data -->
+  <?php include 'includes/scripts.php'; ?>
+  <script>
+    $(function () {
+      var barChartCanvas = $('#barChart').get(0).getContext('2d')
+      var barChart = new Chart(barChartCanvas)
+      var barChartData = {
+        labels: <?php echo $months; ?>,
+        datasets: [
+          {
+            label: 'Borrow',
+            fillColor: 'rgba(210, 214, 222, 1)',
+            strokeColor: 'rgba(210, 214, 222, 1)',
+            pointColor: 'rgba(210, 214, 222, 1)',
+            pointStrokeColor: '#c1c7d1',
+            pointHighlightFill: '#fff',
+            pointHighlightStroke: 'rgba(220,220,220,1)',
+            data: <?php echo $borrow; ?>
+          },
+          {
+            label: 'Return',
+            fillColor: 'rgba(60,141,188,0.9)',
+            strokeColor: 'rgba(60,141,188,0.8)',
+            pointColor: '#3b8bba',
+            pointStrokeColor: 'rgba(60,141,188,1)',
+            pointHighlightFill: '#fff',
+            pointHighlightStroke: 'rgba(60,141,188,1)',
+            data: <?php echo $return; ?>
+          }
+        ]
       }
-    ]
-  }
-  barChartData.datasets[1].fillColor   = '#00a65a'
-  barChartData.datasets[1].strokeColor = '#00a65a'
-  barChartData.datasets[1].pointColor  = '#00a65a'
-  var barChartOptions                  = {
-    //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
-    scaleBeginAtZero        : true,
-    //Boolean - Whether grid lines are shown across the chart
-    scaleShowGridLines      : true,
-    //String - Colour of the grid lines
-    scaleGridLineColor      : 'rgba(0,0,0,.05)',
-    //Number - Width of the grid lines
-    scaleGridLineWidth      : 1,
-    //Boolean - Whether to show horizontal lines (except X axis)
-    scaleShowHorizontalLines: true,
-    //Boolean - Whether to show vertical lines (except Y axis)
-    scaleShowVerticalLines  : true,
-    //Boolean - If there is a stroke on each bar
-    barShowStroke           : true,
-    //Number - Pixel width of the bar stroke
-    barStrokeWidth          : 2,
-    //Number - Spacing between each of the X value sets
-    barValueSpacing         : 5,
-    //Number - Spacing between data sets within X values
-    barDatasetSpacing       : 1,
-    //String - A legend template
-    legendTemplate          : '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].fillColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>',
-    //Boolean - whether to make the chart responsive
-    responsive              : true,
-    maintainAspectRatio     : true
-  }
+      barChartData.datasets[1].fillColor = '#00a65a'
+      barChartData.datasets[1].strokeColor = '#00a65a'
+      barChartData.datasets[1].pointColor = '#00a65a'
+      var barChartOptions = {
+        //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+        scaleBeginAtZero: true,
+        //Boolean - Whether grid lines are shown across the chart
+        scaleShowGridLines: true,
+        //String - Colour of the grid lines
+        scaleGridLineColor: 'rgba(0,0,0,.05)',
+        //Number - Width of the grid lines
+        scaleGridLineWidth: 1,
+        //Boolean - Whether to show horizontal lines (except X axis)
+        scaleShowHorizontalLines: true,
+        //Boolean - Whether to show vertical lines (except Y axis)
+        scaleShowVerticalLines: true,
+        //Boolean - If there is a stroke on each bar
+        barShowStroke: true,
+        //Number - Pixel width of the bar stroke
+        barStrokeWidth: 2,
+        //Number - Spacing between each of the X value sets
+        barValueSpacing: 5,
+        //Number - Spacing between data sets within X values
+        barDatasetSpacing: 1,
+        //String - A legend template
+        legendTemplate: '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].fillColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>',
+        //Boolean - whether to make the chart responsive
+        responsive: true,
+        maintainAspectRatio: true
+      }
 
-  barChartOptions.datasetFill = false
-  var myChart = barChart.Bar(barChartData, barChartOptions)
-  document.getElementById('legend').innerHTML = myChart.generateLegend();
-});
-</script>
-<script>
-$(function(){
-  $('#select_year').change(function(){
-    window.location.href = 'home.php?year='+$(this).val();
-  });
-});
-</script>
+      barChartOptions.datasetFill = false
+      var myChart = barChart.Bar(barChartData, barChartOptions)
+      document.getElementById('legend').innerHTML = myChart.generateLegend();
+    });
+  </script>
+  <script>
+    $(function () {
+      $('#select_year').change(function () {
+        window.location.href = 'home.php?year=' + $(this).val();
+      });
+    });
+  </script>
 </body>
+
 </html>
--- admin/login.php
+++ admin/login.php
@@ -1,32 +1,30 @@
 <?php
-	session_start();
-	include 'includes/conn.php';
+session_start();
+include 'includes/conn.php';
 
-	if(isset($_POST['login'])){
-		$username = $_POST['username'];
-		$password = $_POST['password'];
+if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-		$sql = "SELECT * FROM admin WHERE username = '$username'";
-		$query = $conn->query($sql);
+    // Use prepared statement to prevent SQL injection
+    $stmt = $conn->prepare("SELECT * FROM admin WHERE username = ?");
+    $stmt->bind_param('s', $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-		if($query->num_rows < 1){
-			$_SESSION['error'] = 'Cannot find account with the username';
-		}
-		else{
-			$row = $query->fetch_assoc();
-			if(password_verify($password, $row['password'])){
-				$_SESSION['admin'] = $row['id'];
-			}
-			else{
-				$_SESSION['error'] = 'Incorrect password';
-			}
-		}
-		
-	}
-	else{
-		$_SESSION['error'] = 'Input admin credentials first';
-	}
+    if ($result->num_rows < 1) {
+        $_SESSION['error'] = 'Cannot find account with the username';
+    } else {
+        $row = $result->fetch_assoc();
+        if (password_verify($password, $row['password'])) {
+            $_SESSION['admin'] = $row['id'];
+        } else {
+            $_SESSION['error'] = 'Incorrect password';
+        }
+    }
+} else {
+    $_SESSION['error'] = 'Input admin credentials first';
+}
 
-	header('location: index.php');
-
+header('location: index.php');
 ?>
