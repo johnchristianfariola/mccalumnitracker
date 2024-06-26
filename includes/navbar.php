42fd4f90a90774f3e0b7d4793143576e212a903d
Histsugaya2003 <johnchristianfariola@gmail.com>
2024-06-26 22:40:13 +0800

    commit

--- includes/navbar.php
+++ includes/navbar.php
@@ -12,18 +12,12 @@
             echo '
     <div class="navbar-nav ms-auto p-4 p-lg-0">
         <a href="index.php" class="nav-item nav-link active">Home</a>
-        <a href="about.html" class="nav-item nav-link">About</a>
-        <a href="courses.html" class="nav-item nav-link">Courses</a>
-        <div class="nav-item dropdown">
-            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
-            <div class="dropdown-menu fade-down m-0">
-                <a href="team.html" class="dropdown-item">Our Team</a>
-                <a href="testimonial.html" class="dropdown-item">Testimonial</a>
-                <a href="404.html" class="dropdown-item">404 Page</a>
-            </div>
-        </div>
-        <a href="contact.html" class="nav-item nav-link">Contact</a>
-        <a href="profile.php" class="nav-item nav-link">' . $alumni['firstname'] . ' ' . $alumni['middlename'] .' ' . $alumni['lastname'] . '</a>
+        <a href="about.html" class="nav-item nav-link">News</a>
+        <a href="courses.html" class="nav-item nav-link">Event</a>
+        <a href="courses.html" class="nav-item nav-link">Job</a>
+        <a href="courses.html" class="nav-item nav-link">Forum</a>
+        <a href="contact.html" class="nav-item nav-link">Gallery</a>
+        <a href="profile.php" class="nav-item nav-link">' . $alumni['firstname'] . ' ' . $alumni['middlename'] . ' ' . $alumni['lastname'] . '</a>
     </div>
     ';
         } else {
@@ -49,4 +43,3 @@
 
     </div>
 </nav>
-
---
+++ userpage/index.php
@@ -0,0 +1,234 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+<?php include 'includes/header.php' ?>
+</head>
+
+<body>
+    <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+    <!-- Start Header Top Area -->
+    <?php include 'includes/navbar.php' ?>
+    <!-- End Header Top Area -->
+    <!-- Mobile Menu start -->
+    <?php include 'includes/mobile_view.php' ?>
+    <!-- Mobile Menu end -->
+    <!-- Main Menu area start-->
+    <div class="main-menu-area mg-tb-40">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <ul class="nav nav-tabs notika-menu-wrap menu-it-icon-pro">
+                        <li class="active"><a data-toggle="tab" href="#Home"><i
+                                    class="notika-icon notika-house"></i>Home</a>
+                        </li>
+                        <li><a data-toggle="tab" href="#mailbox"><i class="notika-icon notika-mail"></i> News</a>
+                        </li>
+                        <li><a data-toggle="tab" href="#Interface"><i class="notika-icon notika-edit"></i> Event</a>
+                        </li>
+                        <li><a data-toggle="tab" href="#Charts"><i class="notika-icon notika-bar-chart"></i> Job</a>
+                        </li>
+                        <li><a data-toggle="tab" href="#Tables"><i class="notika-icon notika-windows"></i> Forum</a>
+                        </li>
+                        <li><a data-toggle="tab" href="#Forms"><i class="notika-icon notika-form"></i> Gallery</a>
+
+                    </ul>
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- End Sale Statistic area-->
+
+    <div class="sale-statistic-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-8 col-sm-7 col-xs-12">
+                    <div class="sale-statistic-inner notika-shadow mg-tb-30">
+                        <div class="curved-inner-pro">
+                            <div class="curved-ctn">
+                                <div class="image-section">
+                                    <img src="img/post/1.jpg" alt="male">
+                                </div>
+                                <div class="info-section">
+                                    <h2>Uploader</h2>
+                                    <i>Time uploaded</i>
+                                </div>
+
+
+                            </div>
+
+                        </div>
+                        <div class="content">
+                            <h1>Greeting</h1>
+                            <p>The MCC Graduation</p>
+                        </div>
+
+                        <img src="../homepage/img/graduation_carousel.png" class="news_post" alt="">
+
+                    </div>
+
+                    <div class="sale-statistic-inner notika-shadow mg-tb-30">
+                        <div class="curved-inner-pro">
+                            <div class="curved-ctn">
+                                <div class="image-section">
+                                    <img src="img/post/1.jpg" alt="male">
+                                </div>
+                                <div class="info-section">
+                                    <h2>Uploader</h2>
+                                    <i>Time uploaded</i>
+                                </div>
+
+
+                            </div>
+
+                        </div>
+                        <div class="content">
+                            <h1>TITLE</h1>
+                            <p>The MCC Graduation</p>
+                        </div>
+
+                        <img src="../homepage/img/graduation_carousel.png" class="news_post" alt="">
+
+                    </div>
+
+                </div>
+
+
+                <div class="col-lg-4 col-md-4 col-sm-5 col-xs-12 ">
+                    <div class=" notika-shadow mg-tb-30 sm-res-mg-t-0 full-height wow fadeInRight " data-wow-delay="0.1s" >
+
+                        <div class="card">
+                            <img src="../homepage/img/team-1.jpg" alt="Event Image" class="event_image">
+                            <div class="card-content">
+                                <hr>
+                                <div class="card-title">Event Title</div>
+                                <div class="card-date">Event Date</div>
+                                <div class="card-description">This is a brief description of the event. It provides key
+                                    details about what the event is about and why it is interesting.</div>
+                                <div class="btn btn-default btn-icon-notika waves-effect " ><i
+                                        class="notika-icon notika-menu">More</i></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class=" notika-shadow mg-tb-30 sm-res-mg-t-0 full-height wow fadeInRight " data-wow-delay="0.4s">
+
+                        <div class="card">
+                            <img src="../homepage/img/team-1.jpg" alt="Event Image" class="event_image">
+                            <div class="card-content">
+                                <div class="card-title">Event Title</div>
+                                <hr>
+                                <div class="card-date">Event Date</div>
+                                <div class="card-description">This is a brief description of the event. It provides key
+                                    details about what the event is about and why it is interesting.</div>
+                                <div class="btn btn-default btn-icon-notika waves-effect"><i
+                                        class="notika-icon notika-menu">More</i></div>
+
+                            </div>
+                        </div>
+
+
+                        <div class=" notika-shadow mg-tb-30 sm-res-mg-t-0 full-height wow fadeInRight " data-wow-delay="0.4s">
+
+                            <div class="card">
+                                <img src="../homepage/img/team-2.jpg" alt="Event Image" class="event_image">
+                                <div class="card-content">
+                                    <div class="card-title">Event Title</div>
+                                    <hr>
+                                    <div class="card-date">Event Date</div>
+                                    <div class="card-description">This is a brief description of the event. It provides
+                                        key details about what the event is about and why it is interesting.</div>
+                                    <div class="btn btn-default btn-icon-notika waves-effect"><i
+                                            class="notika-icon notika-menu">More</i></div>
+
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        
+
+        <!-- Start Footer area-->
+
+        <!-- End Footer area-->
+        <!-- jquery
+		============================================ -->
+        <script src="js/vendor/jquery-1.12.4.min.js"></script>
+        <!-- bootstrap JS
+		============================================ -->
+        <script src="js/bootstrap.min.js"></script>
+        <!-- wow JS
+		============================================ -->
+        <script src="js/wow.min.js"></script>
+        <!-- price-slider JS
+		============================================ -->
+        <script src="js/jquery-price-slider.js"></script>
+        <!-- owl.carousel JS
+		============================================ -->
+        <script src="js/owl.carousel.min.js"></script>
+        <!-- scrollUp JS
+		============================================ -->
+        <script src="js/jquery.scrollUp.min.js"></script>
+        <!-- meanmenu JS
+		============================================ -->
+        <script src="js/meanmenu/jquery.meanmenu.js"></script>
+        <!-- counterup JS
+		============================================ -->
+        <script src="js/counterup/jquery.counterup.min.js"></script>
+        <script src="js/counterup/waypoints.min.js"></script>
+        <script src="js/counterup/counterup-active.js"></script>
+        <!-- mCustomScrollbar JS
+		============================================ -->
+        <script src="js/scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
+        <!-- jvectormap JS
+		============================================ -->
+        <script src="js/jvectormap/jquery-jvectormap-2.0.2.min.js"></script>
+        <script src="js/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+        <script src="js/jvectormap/jvectormap-active.js"></script>
+        <!-- sparkline JS
+		============================================ -->
+        <script src="js/sparkline/jquery.sparkline.min.js"></script>
+        <script src="js/sparkline/sparkline-active.js"></script>
+        <!-- sparkline JS
+		============================================ -->
+        <script src="js/flot/jquery.flot.js"></script>
+        <script src="js/flot/jquery.flot.resize.js"></script>
+        <script src="js/flot/curvedLines.js"></script>
+        <script src="js/flot/flot-active.js"></script>
+        <!-- knob JS
+		============================================ -->
+        <script src="js/knob/jquery.knob.js"></script>
+        <script src="js/knob/jquery.appear.js"></script>
+        <script src="js/knob/knob-active.js"></script>
+        <!--  wave JS
+		============================================ -->
+        <script src="js/wave/waves.min.js"></script>
+        <script src="js/wave/wave-active.js"></script>
+        <!--  todo JS
+		============================================ -->
+        <script src="js/todo/jquery.todo.js"></script>
+        <!-- plugins JS
+		============================================ -->
+        <script src="js/plugins.js"></script>
+        <!--  Chat JS
+		============================================ -->
+        <script src="js/chat/moment.min.js"></script>
+        <script src="js/chat/jquery.chat.js"></script>
+        <!-- main JS
+		============================================ -->
+        <script src="js/main.js"></script>
+        <!-- tawk chat JS
+		============================================ -->
+        <script src="js/tawk-chat.js"></script>
+</body>
+
+</html>
