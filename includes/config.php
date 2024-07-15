183c19851582bf352867b105cd53770d9381a8d7
Histsugaya2003 <johnchristianfariola@gmail.com>
2024-07-15 12:27:33 +0800

    commit

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+
+<?php
+define('EMAIL_USERNAME', 'montgomeryaurelia06@gmail.com');
+define('EMAIL_PASSWORD', 'zqhj hxqd ofmj djtv');
+
+
+$databaseURL = "https://mccnians-bc4f4-default-rtdb.firebaseio.com";
+?>
