0bc409bc892956a24c612ad4134f56895ed625ba
Histsugaya2003 <johnchristianfariola@gmail.com>
2024-07-15 12:08:07 +0800

    commit

--- includes/navbar.php
+++ includes/navbar.php
@@ -31,7 +31,7 @@
             <div class="nav-item dropdown">
                 <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
                 <div class="dropdown-menu fade-down m-0">
-                    <a href="team.html" class="dropdown-item">In</a>
+                    <a href="team.html" class="dropdown-item">In Memoriam</a>
                     <a href="testimonial.html" class="dropdown-item">Testimonial</a>
                 </div>
             </div>
