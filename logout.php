1579c6299adab0ce90f2885b2900cafea8bb77bc
Histsugaya2003 <johnchristianfariola@gmail.com>
2024-07-18 09:13:43 +0800

    commit

--- logout.php
+++ logout.php
@@ -1,6 +1,13 @@
 <?php
-	session_start();
-	session_destroy();
+session_start();
 
-	header('location: index.php');
+// Unset all of the session variables related to the user account
+unset($_SESSION['alumni']);
+unset($_SESSION['user']);
+unset($_SESSION['alumni_id']);
+unset($_SESSION['token']);
+
+// Redirect to the index page
+header('location: index.php');
+exit();
 ?>
