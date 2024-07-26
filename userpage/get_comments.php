3724abb7204839e1731dab6b297115b9553a3312
Histsugaya2003 <johnchristianfariola@gmail.com>
2024-07-26 23:28:04 +0800

    comment

--- userpage/get_comments.php
+++ userpage/get_comments.php
@@ -10,63 +10,75 @@ $event_id = $_GET['event_id'];
 $commentData = $firebase->retrieve("comment");
 $commentData = json_decode($commentData, true);
 
-// Filter comments for this event and generate HTML
-$html = '';
-foreach ($commentData as $commentId => $comment) {
-    if ($comment['event_id'] === $event_id) {
-        $commenterData = $firebase->retrieve("alumni/{$comment['alumni_id']}");
-        $commenterData = json_decode($commenterData, true);
-        $commenterProfileUrl = $commenterData['profile_url'];
-        $commenterFirstName = $commenterData['firstname'];
-        $commenterLastName = $commenterData['lastname'];
+// Check if there are any comments
+if (empty($commentData) || !is_array($commentData)) {
+    echo '<li id="no-comments-message" class="center-message">Be the First to Comment</li>';
+} else {
+    // Filter comments for this event and generate HTML
+    $html = '';
+    foreach ($commentData as $commentId => $comment) {
+        if (isset($comment['event_id']) && $comment['event_id'] === $event_id) {
+            $commenterData = $firebase->retrieve("alumni/{$comment['alumni_id']}");
+            $commenterData = json_decode($commenterData, true);
+            $commenterProfileUrl = $commenterData['profile_url'] ?? 'default_profile_url.jpg';
+            $commenterFirstName = $commenterData['firstname'] ?? 'Unknown';
+            $commenterLastName = $commenterData['lastname'] ?? 'User';
 
-        $html .= '<li data-comment-id="' . $commentId . '">
-            <div class="comment-main-level">
-                <div class="comment-avatar"><img src="' . $commenterProfileUrl . '" alt=""></div>
-                <div class="comment-box">
-                    <div class="comment-head">
-                        <h6 class="comment-name by-author">
-                            <a href="#">' . $commenterFirstName . ' ' . $commenterLastName . '</a>
-                        </h6>
-                        <span>' . timeAgo($comment['date_commented']) . '</span>
-                        <i class="fa fa-reply reply-button"></i>
-                        <i class="fa fa-heart"></i>
-                    </div>
-                    <div class="comment-content">
-                        ' . htmlspecialchars($comment['comment']) . '
-                    </div>
-                </div>
-            </div>
-            <div class="reply-container" style="display: none;"></div>
-            <ul class="comments-list reply-list">';
-
-        if (isset($comment['replies'])) {
-            foreach ($comment['replies'] as $replyId => $reply) {
-                $replyAuthorData = $firebase->retrieve("alumni/{$reply['alumni_id']}");
-                $replyAuthorData = json_decode($replyAuthorData, true);
-
-                $html .= '<li>
-                    <div class="comment-avatar"><img src="' . $replyAuthorData['profile_url'] . '" alt=""></div>
+            $html .= '<li data-comment-id="' . $commentId . '">
+                <div class="comment-main-level">
+                    <div class="comment-avatar"><img src="' . $commenterProfileUrl . '" alt=""></div>
                     <div class="comment-box">
                         <div class="comment-head">
                             <h6 class="comment-name by-author">
-                                <a href="#">' . $replyAuthorData['firstname'] . ' ' . $replyAuthorData['lastname'] . '</a>
+                                <a href="#">' . $commenterFirstName . ' ' . $commenterLastName . '</a>
                             </h6>
-                            <span>' . timeAgo($reply['date_replied']) . '</span>
+                            <span>' . timeAgo($comment['date_commented']) . '</span>
+                            <i class="fa fa-reply reply-button"></i>
+                            <i class="fa fa-heart"></i>
                         </div>
                         <div class="comment-content">
-                            ' . htmlspecialchars($reply['comment']) . '
+                            ' . htmlspecialchars($comment['comment']) . '
                         </div>
                     </div>
-                </li>';
-            }
-        }
+                </div>
+                <div class="reply-container" style="display: none;"></div>
+                <ul class="comments-list reply-list">';
 
-        $html .= '</ul></li>';
+            if (isset($comment['replies']) && is_array($comment['replies'])) {
+                foreach ($comment['replies'] as $replyId => $reply) {
+                    $replyAuthorData = $firebase->retrieve("alumni/{$reply['alumni_id']}");
+                    $replyAuthorData = json_decode($replyAuthorData, true);
+                    $replyAuthorProfileUrl = $replyAuthorData['profile_url'] ?? 'default_profile_url.jpg';
+                    $replyAuthorFirstName = $replyAuthorData['firstname'] ?? 'Unknown';
+                    $replyAuthorLastName = $replyAuthorData['lastname'] ?? 'User';
+
+                    $html .= '<li>
+                        <div class="comment-avatar"><img src="' . $replyAuthorProfileUrl . '" alt=""></div>
+                        <div class="comment-box">
+                            <div class="comment-head">
+                                <h6 class="comment-name by-author">
+                                    <a href="#">' . $replyAuthorFirstName . ' ' . $replyAuthorLastName . '</a>
+                                </h6>
+                                <span>' . timeAgo($reply['date_replied']) . '</span>
+                            </div>
+                            <div class="comment-content">
+                                ' . htmlspecialchars($reply['comment']) . '
+                            </div>
+                        </div>
+                    </li>';
+                }
+            }
+
+            $html .= '</ul></li>';
+        }
+    }
+
+    if (empty($html)) {
+        echo '<li id="no-comments-message" class="center-message">Be the First to Comment</li>';
+    } else {
+        echo $html;
     }
 }
-
-echo $html;
 
 function timeAgo($dateString) {
     $date = new DateTime($dateString);
