657a904d6a615846ae48782172aa5a5a1bd4792f
Histsugaya2003 <johnchristianfariola@gmail.com>
2024-07-30 08:23:05 +0800

    commit

--- userpage/visit_job.php
+++ userpage/visit_job.php
@@ -102,7 +102,7 @@
                             </div>
                             <br><br>
                             <img style="border-radius: 1rem"
-                                src="../admin/<?= htmlspecialchars($jobDetails['imagePath']) ?>" class="news_post"
+                                src="../admin/<?= htmlspecialchars($jobDetails['imagePath']) ?>" class="news_post" style="height:90vh;"
                                 alt="news image">
                         <?php else: ?>
                             <p>Job not found.</p>
