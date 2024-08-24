5382f3582ef0d3032613bb19013374f27dfa5287
Histsugaya2003 <johnchristianfariola@gmail.com>
2024-08-24 21:46:43 +0800

    commit

--- userpage/includes/navbar.php
+++ userpage/includes/navbar.php
@@ -130,7 +130,7 @@ $alumni_data = json_decode($alumni_data, true);
             <hr>
             <div class="settings-links">
                 <img src="../images/setting.png" class="settings-icon">
-                <a href="trialhome.php">Settings & Privacy <img src="../images/arrow.png" width="10px"></a>
+                <a href="view_profile.php">Settings & Privacy <img src="../images/arrow.png" width="10px"></a>
             </div>
             <div class="settings-links">
                 <img src="../images/help.png" class="settings-icon">
