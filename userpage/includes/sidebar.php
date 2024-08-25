9752eb5dc0af4413300e8c8632ffcfeaeeef8867
Histsugaya2003 <johnchristianfariola@gmail.com>
2024-08-26 00:34:45 +0800

    commit

--- userpage/includes/sidebar.php
+++ userpage/includes/sidebar.php
@@ -1,8 +1,8 @@
 <div class="sidebar">
         <h2>Profile Menu</h2>
         <ul>
-            <li><a href="#personal-info">Personal Info</a></li>
-            <li><a href="#update">Update Account</a></li>
+            <li><a href="profile_overview.php">Personal Info</a></li>
+            <li><a href="update_account.php">Update Account</a></li>
 
             <li><a href="#update-password">Update Password</a></li>
             <li><a href="#delete-account">Delete Account</a></li>
