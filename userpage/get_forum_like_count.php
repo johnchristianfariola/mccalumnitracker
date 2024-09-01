62dbb01f02bb1aa3afc75046eec2d2bfeec2506e
Histsugaya2003 <johnchristianfariola@gmail.com>
2024-09-01 15:09:00 +0800

    commit

--- userpage/get_forum_like_count.php
+++ userpage/get_forum_like_count.php
@@ -10,7 +10,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $forum_id = $_GET['forum_id'];
     $alumni_id = $_SESSION['user']['id'];
 
-
     // Get current forum data
     $forum_data = $firebase->retrieve("forum/$forum_id");
     $forum_data = json_decode($forum_data, true);
@@ -27,10 +26,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         }
     }
 
+    // Get current timestamp
+    $current_timestamp = date('Y-m-d H:i:s');
+    $formatted_date = date('F j, Y, g:i a');
+
     echo json_encode([
         'status' => 'success',
         'reaction_counts' => $reaction_counts,
-        'user_reaction' => $user_reaction
+        'user_reaction' => $user_reaction,
+        'timestamp' => $current_timestamp,
+        'formatted_date' => $formatted_date
     ]);
 } else {
     echo json_encode(['status' => 'error', 'message' => 'Invalid request method']);
