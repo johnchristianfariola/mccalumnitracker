234118c633d2979064a550effab0b8775dd4b208
Histsugaya2003 <johnchristianfariola@gmail.com>
2024-09-04 16:41:39 +0800

    commit

--- userpage/update_notification_check.php
+++ userpage/update_notification_check.php
@@ -4,10 +4,15 @@ require_once '../includes/config.php';
 
 $firebase = new firebaseRDB($databaseURL);
 
+// Set the timezone to the Philippines
+date_default_timezone_set('Asia/Manila');
+
 $data = json_decode(file_get_contents('php://input'), true);
 $user_id = $data['user_id'];
 
+// Get the current timestamp in the Philippines timezone
 $timestamp = date('Y-m-d H:i:s');
+
 $firebase->update($table, "alumni/" . $user_id, ["last_notification_check" => $timestamp]);
 
 echo json_encode(['success' => true]);
