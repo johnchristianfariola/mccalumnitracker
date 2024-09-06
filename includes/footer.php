f8cf6876652682754c160f29e63d019fe6456448
Histsugaya2003 <johnchristianfariola@gmail.com>
2024-09-06 11:47:07 +0800

    commit

--- includes/footer.php
+++ includes/footer.php
@@ -1,78 +1,81 @@
+<?php
+// Include necessary files
+require_once 'includes/firebaseRDB.php';
+require_once 'includes/config.php';
+
+// Initialize Firebase
+$databaseURL = "https://mccnians-bc4f4-default-rtdb.firebaseio.com";
+$firebase = new firebaseRDB($databaseURL);
+
+// Retrieve gallery data
+$galleryData = $firebase->retrieve("gallery");
+$galleryData = json_decode($galleryData, true);
+
+// Sort gallery data by creation date in descending order
+usort($galleryData, function ($a, $b) {
+    return strtotime($b['created_on']) - strtotime($a['created_on']);
+});
+?>
 <div class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
-        <div class="container py-5">
-            <div class="row g-5">
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-white mb-3">Quick Link</h4>
-                    <a class="btn btn-link" href="">About Us</a>
-                    <a class="btn btn-link" href="">Contact Us</a>
-                    <a class="btn btn-link" href="">Privacy Policy</a>
-                    <a class="btn btn-link" href="">Terms & Condition</a>
-                    <a class="btn btn-link" href="">FAQs & Help</a>
+    <div class="container py-5">
+        <div class="row g-5">
+            <div class="col-lg-3 col-md-6">
+                <h4 class="text-white mb-3">Quick Link</h4>
+                <a class="btn btn-link" href="">About Us</a>
+                <a class="btn btn-link" href="">Contact Us</a>
+                <a class="btn btn-link" href="">Privacy Policy</a>
+                <a class="btn btn-link" href="">Terms & Condition</a>
+                <a class="btn btn-link" href="">FAQs & Help</a>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h4 class="text-white mb-3">Contact</h4>
+                <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>09096437668</p>
+                <p class="mb-2"><i class="fa fa-envelope me-3"></i>johnchristianfariola@example.com</p>
+                <div class="d-flex pt-2">
+                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
+                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
+                    <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
+                </div> 
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h4 class="text-white mb-3">Gallery</h4>
+                <div class="row g-2 pt-2">
+                    <?php foreach ($galleryData as $galleryItem): ?>
+                        <?php if (isset($galleryItem['image_url']) && !empty($galleryItem['image_url'])): ?>
+                            <div class="col-4">
+                                <img class="img-fluid bg-light p-1" src="admin/<?php echo htmlspecialchars($galleryItem['image_url']); ?>" alt="<?php echo htmlspecialchars($galleryItem['gallery_name']); ?>">
+                            </div>
+                        <?php endif; ?>
+                    <?php endforeach; ?>
                 </div>
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-white mb-3">Contact</h4>
-                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>09096437668</p>
-                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>johnchristianfariola@example.com</p>
-                    <div class="d-flex pt-2">
-                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
-                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
-                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
-                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
-                    </div> 
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-white mb-3">Gallery</h4>
-                    <div class="row g-2 pt-2">
-                        <div class="col-4">
-                            <img class="img-fluid bg-light p-1" src="homepage/img/profile.jpg" alt="">
-                        </div>
-                        <div class="col-4">
-                            <img class="img-fluid bg-light p-1" src="homepage/img/profile.jpg" alt="">
-                        </div>
-                        <div class="col-4">
-                            <img class="img-fluid bg-light p-1" src="homepage/img/profile.jpg" alt="">
-                        </div>
-                        <div class="col-4">
-                            <img class="img-fluid bg-light p-1" src="homepage/img/profile.jpg" alt="">
-                        </div>
-                        <div class="col-4">
-                            <img class="img-fluid bg-light p-1" src="homepage/img/profile.jpg" alt="">
-                        </div>
-                        <div class="col-4">
-                            <img class="img-fluid bg-light p-1" src="homepage/img/profile.jpg" alt="">
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-white mb-3">Newsletter</h4>
-                    <p></p>
-                    <div class="position-relative mx-auto" style="max-width: 400px;">
-                        
-                        <button type="button"
-                            class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2 openFormButton">SignUp</button>
-                    </div>
+            </div>
+            <div class="col-lg-3 col-md-6">
+                <h4 class="text-white mb-3">Newsletter</h4>
+                <p></p>
+                <div class="position-relative mx-auto" style="max-width: 400px;">
+                    <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2 openFormButton">SignUp</button>
                 </div>
             </div>
         </div>
-        <div class="container">
-            <div class="copyright">
-                <div class="row">
-                    <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                        &copy; <a class="border-bottom" href="#">Mcc Alumni Tracker</a>, All Right Reserved.
-
-                        <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-                        Designed By <a class="border-bottom" href="">John Christian Fariola</a><br><br>
-                        Distributed By <a class="border-bottom" href="">IT DEPARTMENT</a>
-                    </div>
-                    <div class="col-md-6 text-center text-md-end">
-                        <div class="footer-menu">
-                            <a href="">Home</a>
-                            <a href="">Cookies</a>
-                            <a href="">Help</a>
-                            <a href="">FQAs</a>
-                        </div>
+    </div>
+    <div class="container">
+        <div class="copyright">
+            <div class="row">
+                <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                    &copy; <a class="border-bottom" href="#">Mcc Alumni Tracker</a>, All Right Reserved.
+                    Designed By <a class="border-bottom" href="">John Christian Fariola</a><br><br>
+                    Distributed By <a class="border-bottom" href="">IT DEPARTMENT</a>
+                </div>
+                <div class="col-md-6 text-center text-md-end">
+                    <div class="footer-menu">
+                        <a href="">Home</a>
+                        <a href="">Cookies</a>
+                        <a href="">Help</a>
+                        <a href="">FQAs</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
