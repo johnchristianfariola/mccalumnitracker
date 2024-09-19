d9a1d8e0b12ea9348160798db47ec434efbd297c
Histsugaya2003 <johnchristianfariola@gmail.com>
2024-09-19 15:28:19 +0800

    commit

--- index.php
+++ index.php
@@ -651,7 +651,38 @@ $data = array_slice($data, 0, 5);
     <!-- Template Javascript -->
     <script src="homepage/js/main.js"></script>
 
+<script>
+    // Disable right-click
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
 
+// Disable F12, Ctrl+Shift+I, Ctrl+Shift+J, Ctrl+U
+document.onkeydown = function(e) {
+    if (
+        e.key === 'F12' ||
+        (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'J')) ||
+        (e.ctrlKey && e.key === 'U')
+    ) {
+        e.preventDefault();
+    }
+};
+
+// Disable developer tools
+function disableDevTools() {
+    if (window.devtools.isOpen) {
+        window.location.href = "about:blank";
+    }
+}
+
+// Check for developer tools every 100ms
+setInterval(disableDevTools, 100);
+
+// Disable selecting text
+document.onselectstart = function(e) {
+    e.preventDefault();
+};
+</script>
     <!-- Modal -->
     <?php include 'includes/auth.php' ?>
 
