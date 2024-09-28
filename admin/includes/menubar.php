3ae02877c056284b88a715754644f7c5a660ab06
Histsugaya2003 <johnchristianfariola@gmail.com>
2024-09-28 20:32:31 +0800

    commit

--- admin/includes/menubar.php
+++ admin/includes/menubar.php
@@ -82,20 +82,6 @@
 
         </ul>
       </li>
-      <li class="treeview">
-        <a href="#">
-          <i class="fa fa-book"></i>
-          <span>Logs</span>
-          <span class="pull-right-container">
-
-            <i class="fa fa-angle-left pull-right"></i>
-          </span>
-        </a>
-        <ul class="treeview-menu">
-          <li><a href="timeline.php"><i class="fa fa-angle-right"></i> Activity</a></li> 
-      
-        </ul>
-      </li>
 
       <!--   <li class="treeview">
         <a href="#">
