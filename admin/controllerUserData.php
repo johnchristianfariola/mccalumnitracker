5dd74a1be06567309582c9592932a81b1288d166
Histsugaya2003 <johnchristianfariola@gmail.com>
2024-10-03 08:10:35 +0800

    commit

--- admin/controllerUserData.php
+++ admin/controllerUserData.php
@@ -164,9 +164,7 @@ if (isset($_POST['check-email'])) {
             } catch (Exception $e) {
                 $errors['otp-error'] = "Message could not be sent. Mailer error: {$mail->ErrorInfo}";
             }
-        } else {
-            $errors['email'] = "This email address does not exist!";
-        }
+        } 
     }
 }
 
