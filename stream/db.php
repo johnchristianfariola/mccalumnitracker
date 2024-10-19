e3f3a2081374bf9b9e756a4209104cb34d8b1478
Histsugaya2003 <johnchristianfariola@gmail.com>
2024-10-19 17:40:02 +0800

    commit

--- stream/db.php
+++ stream/db.php
@@ -1,8 +1,5 @@
 <?php
-$servername = "127.0.0.1"; // e.g., 'localhost'
-$username = "u510162695_judging_root";
-$password = "1Judging_root";
-$dbname = "u510162695_judging";
+
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
