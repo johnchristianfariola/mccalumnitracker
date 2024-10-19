1a9472ffa7ea2c383b22283745cf3d07d6f6c2db
Histsugaya2003 <johnchristianfariola@gmail.com>
2024-10-19 17:00:21 +0800

    commit

--- stream/index.php
+++ stream/index.php
@@ -12,17 +12,6 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// SQL to alter table and add image_url column
-$alterTableSql = "ALTER TABLE `live_streams` 
-                  ADD COLUMN `image_url` VARCHAR(255) AFTER `role`,
-                  MODIFY `host_uid` varchar(50) DEFAULT NULL";
-
-if ($conn->query($alterTableSql) === TRUE) {
-    echo "Table 'live_streams' altered successfully to add image_url column and modify host_uid<br>";
-} else {
-    echo "Error altering table: " . $conn->error . "<br>";
-}
-
 // SQL to insert data
 $insertDataSql = "INSERT INTO `live_streams` (`stream_id`, `organizer_id`, `channel_name`, `stream_title`, `host_uid`, `stream_status`, `start_time`, `end_time`, `viewer_count`, `app_id`, `token`, `role`, `image_url`, `created_at`, `updated_at`, `is_video_enabled`, `is_audio_enabled`) VALUES
 (11, 31, 'Channel_1', 'Tech Talk', NULL, 'scheduled', '2024-10-18 16:06:00', '2024-10-18 18:06:00', 0, '639e26f0457a4e85b9e24844db6078cd', 'f390d604df0f4e0191dc0652773f77a3', 'host', 'https://picsum.photos/1920/1080?random=1', '2024-10-19 02:38:56', '2024-10-19 08:19:44', 1, 1)";
