43e4839c0c733e6cec07f13ba52a6adb9d11c6d0
Histsugaya2003 <johnchristianfariola@gmail.com>
2024-11-01 16:37:08 +0800

    commit

--- admin/job.php
+++ admin/job.php
@@ -66,7 +66,7 @@
                 <div class="table-responsive">
                   <table id="example1" class="table table-bordered">
                     <thead>
-                      <th>tatus</th>
+                      <th>Status</th>
                       <th width="15%">Work Type</th>
                       <th>Job Title</th>
                       <th>Company</th>
